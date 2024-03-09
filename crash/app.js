37d8912cabab5d60dd0dae74d34229ed27b181d6
정민영 <myjeong19@naver.com>
2024-03-09 18:54:39 +0900

    Feat. Screen size 정의

---
+++ crash/app.js
@@ -0,0 +1,23 @@
+class App {
+  constructor() {
+    this.canvas = document.createElement('canvas');
+    this.ctx = this.canvas.getContext('2d');
+
+    document.body.appendChild(this.canvas);
+    window.addEventListener('resize', this.resize.bind(this), false);
+  }
+
+  //   screen size
+  resize() {
+    this.stageWidth = document.body.clientWidth;
+    this.stageHeight = document.body.clientHeight;
+
+    this.canvas.width = this.stageWidth * 2;
+    this.canvas.height = this.stageHeight * 2;
+    this.ctx.scale(2, 2);
+  }
+}
+
+window.onload = () => {
+  new App();
+};
